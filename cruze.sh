0416fb694af37ba3fd7660abbaa0479de366cf46
Yo <nix@yo.local>
2020-07-18 14:22:30 +0600

    slugify /dir

--- cruze.sh
+++ cruze.sh
@@ -9,6 +9,8 @@ fi
 
 if [ -z "$2" ]; then
   dir=$(date '+%Y-%m-%d')
+else
+  dir=$(echo "$dir" | iconv -t ascii//TRANSLIT | sed -r s/[~\^]+//g | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z)
 fi
 
 logo(){

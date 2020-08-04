d7edee57734f5ce770d5be3aaefae338284d018f
nagarajcruze <rajnagaraj386@gmail.com>
2020-08-04 21:21:48 +0530

    added rapiddns and some UI changes

--- cruze.sh
+++ cruze.sh
@@ -14,13 +14,13 @@ echo $'\e[1;31m'"
 }
 
 initDefaults(){
-  echo $1
   domain=$1
   if [ -z "$1" ]; then
-    echo "No argument supplied"
-    echo "Domain Name requried! Ex: ./cruze.sh example.com"
+    echo -e "\e[91mNo argument supplied\e[0m"
+    echo -e "\e[91mDomain Name requried! \e[0m Ex: ./cruze.sh example.com"
     exit 1
   fi
+  echo -e "\e[96mThe Target is \e[0m \e[96m$1\e[0m"
   dir=$1-$(date '+%Y-%m-%d')
   dir=$(echo "$dir" | sed -r s/[^a-zA-Z0-9]+/_/g | tr A-Z a-z)
   mkdir -p $dir
@@ -28,34 +28,40 @@ initDefaults(){
 
 assetFinder(){
   # assetfinder
-  echo "-------------------Assetfinder Started  -------------------------------------------"
+  echo -e "\e[91m-------------------Assetfinder Started  -------------------------------------------\e[0m"
   assetfinder --subs-only $domain | tee $dir/asset_subs.txt
 }
 
 subLister(){
   # sublister
-  echo "-------------------Sublister Started  ----------------------------------------------"
-  python3 ~/tools/Sublist3r/sublist3r.py -v -t 10 -d $domain -o $dir/subs.txt
+  echo -e "\e[91m-------------------Sublister Started  ----------------------------------------------\e[0m"
+  python3 ~/tools/Sublist3r/sublist3r.py -t 10 -d $domain -o $dir/subs.txt
 }
 
 subFinder(){
   # subfinder
-  echo "------------------Subfinder---------------------------------------------------------"
+  echo -e "\e[91m-------------------Subfinder---------------------------------------------------------\e[0m"
   subfinder -d $domain --silent -o $dir/subfinder.txt
 }
 
+rapiddns(){
+  echo -e "\e[91m-------------------Rapiddns-----------------------------------------------------------\e[0m"
+  curl -s "https://rapiddns.io/subdomain/$domain?full=1"| grep -oP '_blank">\K[^<]*' | grep -v http | sort -u | tee $dir/rapiddns.txt
+}
+
 groupSubdomains(){
-  cat $dir/asset_subs.txt $dir/subs.txt $dir/subfinder.txt | sort -u > $dir/subdomains.txt
+  cat $dir/asset_subs.txt $dir/subs.txt $dir/subfinder.txt $dir/rapiddns.txt | sort -u > $dir/subdomains.txt
   rm $dir/asset_subs.txt
   rm $dir/subs.txt
   rm $dir/subfinder.txt
+  rm $dir/rapiddns.txt
 }
 
 aquaTone(){
   # aquatone
-  echo "Now aquatone will start to screenshot and some extra recons."
+  echo -e "\e[91mNow aquatone will start to screenshot and some extra recons."
   cat $dir/subdomains.txt | aquatone -chrome-path /usr/bin/chromium -ports xlarge -out $dir/
-  echo "-------------------Aquatone Scan Completed------------------------------------------"
+  echo -e "\e[91m-------------------Aquatone Scan Completed------------------------------------------\e[0m"
 }
 
 liveSubdomains(){
@@ -65,26 +71,26 @@ liveSubdomains(){
 
 nmapScan(){
   # nmap scripts
-  echo "-------------------Now Nmap will ping for IP addresses-------------------------------"
+  echo -e "\e[91m-------------------Now Nmap will ping for IP addresses-------------------------------\e[0m"
   nmap -iL $dir/subdomains.txt -Pn -n -sn -oG $dir/nmap_live_ip.txt
   cat $dir/nmap_live_ip.txt | grep ^Host | cut -d " " -f 2 > $dir/live_ip.txt
   rm $dir/nmap_live_ip.txt
 }
 
 pathFinders(){
-  echo "-------------------gau Scan Started--------------------------------------------------"
+  echo -e "\e[91m-------------------gau Scan Started--------------------------------------------------\e[0m"
   gau --subs $domain | tee $dir/gau_urls.txt
 
-  echo "-------------------hakrawler Started-------------------------------------------------"
+  echo -e "\e[91m-------------------hakrawler Started-------------------------------------------------\e[0m"
   cat $dir/subdomains.txt | hakrawler -depth 3 -plain | tee $dir/hakrawler.txt
 
-  echo "-------------------waybackurls Scan Started------------------------------------------"
+  echo -e "\e[91m-------------------waybackurls Scan Started------------------------------------------\e[0m"
   cat $dir/subdomains.txt | waybackurls | tee $dir/archiveurl.txt
   cat $dir/aquatone_urls.txt $dir/gau_urls.txt $dir/archiveurl.txt $dir/hakrawler.txt | sort -u > $dir/waybackurls.txt
 }
 
 scanSuspect(){
-  echo "-------------------looking for vulnerable endpoints----------------------------------"
+  echo -e "\e[91m-------------------looking for vulnerable endpoints----------------------------------\e[0m"
   mkdir $dir/paramlist
   cat $dir/waybackurls.txt | gf redirect > $dir/paramlist/redirect.txt
   cat $dir/waybackurls.txt | gf ssrf > $dir/paramlist/ssrf.txt
@@ -96,7 +102,7 @@ scanSuspect(){
   cat $dir/waybackurls.txt | gf debug_logic > $dir/paramlist/debug_logic.txt
   cat $dir/waybackurls.txt | gf interestingsubs > $dir/paramlist/interestingsubs.txt
   cat $dir/waybackurls.txt | grep "=" | tee $dir/domainParam.txt
-  echo "-------------------Gf patters Scan Completed------------------------------------------------"
+  echo -e "\e[91m-------------------Gf patters Scan Completed------------------------------------------------\e[0m"
 }
 
 wafDetect(){
@@ -119,7 +125,7 @@ end(){
 
   echo "sudo masscan -iL $dir/live_ip.txt -p 1-65535 --rate 10000 -oJ $dir/masscan_output.json"
 
-  echo "python3 ~/tools/dirsearch/dirsearch.py -L subdomains.txt -e php,asp,aspx,jsp,html,zip,jar  --plain-text-report=dir_results.txt"
+  echo "python3 ~/tools/dirsearch/dirsearch.py -L $dir/subdomains.txt -e php,asp,aspx,jsp,html,zip  --plain-text-report=dir_results.txt"
 
 }
 
@@ -131,6 +137,7 @@ initDefaults "$1"
 assetFinder
 subLister
 subFinder
+rapiddns
 groupSubdomains
 aquaTone
 liveSubdomains

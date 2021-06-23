59719049c0d87279d6bf48298ac09e0437323bad
Nagaraj Cruze <rajnagaraj386@gmail.com>
2021-06-23 11:32:53 +0530

    Added some extra tools

--- dependecies.sh
+++ dependecies.sh
@@ -5,17 +5,27 @@ then
     exit 1
 fi
 
-if [[ -z "$GOPATH" ]];then
-                echo "You don't have Golang installed, please install it."
-                echo "https://golang.org/dl/"
-                echo "Once Installed, add Go to your path by these commands."
-                echo 'export GOPATH=$HOME/go	>> ~/.bash_profile'
-				echo 'export PATH=$GOPATH/bin:$PATH >> ~/.bash_profile'
-fi
+# if [[ -z "$GOPATH" ]];then
+#                 echo "You don't have Golang installed, please install it."
+#                 echo "https://golang.org/dl/"
+#                 echo "Once Installed, add Go to your path by these commands."
+#                 echo 'export GOPATH=$HOME/go	>> ~/.bash_profile'
+# 				echo 'export PATH=$GOPATH/bin:$PATH >> ~/.bash_profile'
+# fi
 
 # Tools
 mkdir -p ~/tools
 cd ~/tools
+
+# Install Go
+RUN wget https://golang.org/dl/go1.15.2.linux-amd64.tar.gz -O /tmp/go.tar.gz
+RUN cd /tmp && tar -C /usr/local -xzf go.tar.gz && cd /
+ENV PATH="${PATH}:/usr/local/go/bin:/root/go/bin:~/go/bin"
+
+# Amass
+RUN wget https://github.com/OWASP/Amass/releases/download/v3.10.3/amass_linux_amd64.zip -O /tmp/amass.zip
+RUN cd /tmp && unzip amass.zip && mv amass_linux_amd64 /tools/amass && cd /
+ENV PATH="${PATH}:~/tools/amass"
 
 # install aquatone
 echo "Installing Aquatone"
@@ -86,10 +96,6 @@ echo "installing Arjun"
 git clone https://github.com/s0md3v/Arjun.git
 echo "done installing Arjun"
 
-echo "instaling subrute"
-go get -v github.com/projectdiscovery/subfinder/cmd/subfinder
-sudo ln -s ~/go/bin/subfinder /usr/bin/
-
 echo "Don't forget to install gau"
 echo https://github.com/lc/gau/releases/
 echo tar xvf gau_1.0.2_linux_amd64.tar.gz
@@ -99,6 +105,23 @@ echo sudo ln -s ~/tools/gau /usr/bin
 echo "instaling Amass"
 go get -v github.com/OWASP/Amass/v3/...
 sudo ln -s ~/go/bin/amass /usr/bin/
+
+# Subfinder
+git clone https://github.com/projectdiscovery/subfinder.git
+cd ~/tools/subfinder/v2/cmd/subfinder && go build . && sudo ln -s $(pwd)/subfinder /usr/bin/ && cd ~/
+
+# github-search
+git clone https://github.com/gwen001/github-search.git ~/tools/github-search
+cd ~/tools/github-search && pip3 install -r requirements2.txt && pip3 install -r requirements3.txt && cd ~/
+
+# masscan
+RUN git clone https://github.com/robertdavidgraham/masscan
+RUN cd ~/tools/masscan && make && sudo ln -s ~/tools/masscan/masscan /usr/bin cd ~/
+ENV PATH="${PATH}:/tools/masscan/bin"
+
+# meg
+go get -u github.com/tomnomnom/meg
+sudo ln -s ~/go/bin/meg /usr/bin
 
 
 echo "if you want download full seclists or download raft folder which is i use mostly."

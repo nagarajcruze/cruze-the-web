3fa757de64d949fe4e6ee6c3dbb45a015eec443f
Nagaraj Cruze <rajnagaraj386@gmail.com>
2021-06-23 11:35:53 +0530

    New Changes

--- dependecies.sh
+++ dependecies.sh
@@ -5,109 +5,119 @@ then
     exit 1
 fi
 
-# if [[ -z "$GOPATH" ]];then
-#                 echo "You don't have Golang installed, please install it."
-#                 echo "https://golang.org/dl/"
-#                 echo "Once Installed, add Go to your path by these commands."
-#                 echo 'export GOPATH=$HOME/go	>> ~/.bash_profile'
-# 				echo 'export PATH=$GOPATH/bin:$PATH >> ~/.bash_profile'
-# fi
-
 # Tools
 mkdir -p ~/tools
 cd ~/tools
 
+#requirements
+apt-get update && apt-get upgrade --yes && apt-get install -y --no-install-recommends unzip sudo jq build-essential tmux gcc iputils-ping git vim wget awscli tzdata curl make nmap whois python python3 python3-pip perl dnsutils net-tools zsh nano sqlmap libldns-dev libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev libgmp-dev zlib1g-dev libpcap-dev && ls /var/lib/apt/lists/ && rm -rf /var/lib/apt/lists/*
+
 # Install Go
-RUN wget https://golang.org/dl/go1.15.2.linux-amd64.tar.gz -O /tmp/go.tar.gz
-RUN cd /tmp && tar -C /usr/local -xzf go.tar.gz && cd /
-ENV PATH="${PATH}:/usr/local/go/bin:/root/go/bin:~/go/bin"
+wget https://golang.org/dl/go1.15.2.linux-amd64.tar.gz -O /tmp/go.tar.gz
+cd /tmp && tar -C /usr/local -xzf go.tar.gz && cd ~/
+echo PATH="${PATH}:/usr/local/go/bin:/root/go/bin:~/go/bin" >> .bashrc
+echo GO111MODULE=on >> .bashrc 
 
-# Amass
-RUN wget https://github.com/OWASP/Amass/releases/download/v3.10.3/amass_linux_amd64.zip -O /tmp/amass.zip
-RUN cd /tmp && unzip amass.zip && mv amass_linux_amd64 /tools/amass && cd /
-ENV PATH="${PATH}:~/tools/amass"
-
-# install aquatone
+# aquatone
 echo "Installing Aquatone"
 go get github.com/michenriksen/aquatone
-sudo ln -s ~/go/bin/aquatone /usr/bin/
 echo "done Installing Aquatone"
 
-
-#install assetfinder
+# assetfinder
 echo "Installing Assetfinder"
 go get -u github.com/tomnomnom/assetfinder
-sudo ln -s ~/go/bin/assetfinder /usr/bin/
 echo "done Installing Assetfinder"
 
-#install httprobe
+# httprobe
 echo "Installing Httprobe"
 go get -u github.com/tomnomnom/httprobe
-sudo ln -s ~/go/bin/httprobe /usr/bin/
 echo "done Installing Httprobe"
 
-#install waybackurls
+# waybackurls
 echo "Installing Waybackurls"
 go get github.com/tomnomnom/waybackurls
-sudo ln -s ~/go/bin/waybackurls /usr/bin/
 echo "done Installing Waybackurls"
 
-#install gf
+# unfurl
+echo "installing unfurl"
+go get -u github.com/tomnomnom/unfurl 
+echo "done installing unfurl"
+
+#ffuf
+echo "installing ffuf"
+go get github.com/ffuf/ffuf
+echo "done installing ffuf"
+
+#hakrawler
+echo "installing hakrawler"
+go get github.com/hakluke/hakrawler
+echo "done installing hakrawler"
+
+# meg
+echo "installing meg"
+go get -u github.com/tomnomnom/meg
+
+# Amass
+echo "instaling Amass"
+go get -v github.com/OWASP/Amass/v3/...
+
+# gau
+echo "Installing gau"
+# GO111MODULE=on 
+go get -u -v github.com/lc/gau
+
+#getJS
+echo "installing getJS"
+go get github.com/003random/getJS
+echo "done installing getJS"
+
+# gf
 echo "Installing gf"
 go get -u github.com/tomnomnom/gf
-sudo ln -s ~/go/bin/gf /usr/bin/
-git clone https://github.com/1ndianl33t/Gf-Patterns
+cd ~/tools/
+git clone https://github.com/1ndianl33t/Gf-Patterns ~/tools/Gf-Patterns
 mkdir ~/.gf
 cp  ~/tools/Gf-Patterns/*.json ~/.gf
 echo "done Installing gf"
 
-echo "installing unfurl"
-go get -u github.com/tomnomnom/unfurl 
-sudo ln -s ~/go/bin/unfurl /usr/bin/
-echo "done installing unfurl"
-
-echo "installing ffuf"
-go get github.com/ffuf/ffuf
-sudo ln -s ~/go/bin/ffuf /usr/bin/
-echo "done installing ffuf"
-
+# dirsearch
 echo "installing dirsearch"
-git clone https://github.com/maurosoria/dirsearch.git
-cd ~/tools/
+git clone https://github.com/maurosoria/dirsearch.git ~/tools/dirsearch
 echo "done installing dirsearch"
 
+# sqlmap
 echo "installing sqlmap"
-git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
-cd ~/tools/
+git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev 
 echo "done installing sqlmap"
 
-echo "installing hakrawler"
-go get github.com/hakluke/hakrawler
-sudo ln -s ~/go/bin/hakrawler /usr/bin/
-echo "done installing hakrawler"
-
+# wafw00f
 echo "installing wafwoof"
-git clone https://github.com/EnableSecurity/wafw00f.git
-cd ~/tools/wafw00f/
-python setup.py install
+git clone https://github.com/EnableSecurity/wafw00f.git ~/tools/wafw00f
+cd ~/tools/wafw00f
+python3 setup.py install
+cd ~/
 echo "done installing wafoof"
 
+# Arjun
 echo "installing Arjun"
-git clone https://github.com/s0md3v/Arjun.git
+git clone https://github.com/s0md3v/Arjun.git ~/tools/Arjun
 echo "done installing Arjun"
 
-echo "Don't forget to install gau"
-echo https://github.com/lc/gau/releases/
-echo tar xvf gau_1.0.2_linux_amd64.tar.gz
-echo mv gau ~/tools/
-echo sudo ln -s ~/tools/gau /usr/bin
+# dnsgen
+echo "installing dnsgen"
+git clone https://github.com/ProjectAnte/dnsgen ~/tools/dnsgen
+cd ~/tools/dnsgen
+pip3 install -r requirements.txt
+python3 setup.py install
+cd ~/
 
-echo "instaling Amass"
-go get -v github.com/OWASP/Amass/v3/...
-sudo ln -s ~/go/bin/amass /usr/bin/
+# sprawl
+echo "installing sprawl"
+https://github.com/tehryanx/sprawl.git ~/tools/sprawl
+echo "done installing sprawl"
 
 # Subfinder
-git clone https://github.com/projectdiscovery/subfinder.git
+git clone https://github.com/projectdiscovery/subfinder.git ~/tools/subfinder
 cd ~/tools/subfinder/v2/cmd/subfinder && go build . && sudo ln -s $(pwd)/subfinder /usr/bin/ && cd ~/
 
 # github-search
@@ -115,19 +125,20 @@ git clone https://github.com/gwen001/github-search.git ~/tools/github-search
 cd ~/tools/github-search && pip3 install -r requirements2.txt && pip3 install -r requirements3.txt && cd ~/
 
 # masscan
-RUN git clone https://github.com/robertdavidgraham/masscan
-RUN cd ~/tools/masscan && make && sudo ln -s ~/tools/masscan/masscan /usr/bin cd ~/
-ENV PATH="${PATH}:/tools/masscan/bin"
+git clone https://github.com/robertdavidgraham/masscan ~/tools/masscan
+cd ~/tools/masscan && make 
+sudo ln -s ~/tools/masscan/bin/masscan /usr/bin
 
-# meg
-go get -u github.com/tomnomnom/meg
-sudo ln -s ~/go/bin/meg /usr/bin
-
+#SecretFinder
+git clone https://github.com/m4ll0k/SecretFinder.git ~/tools/secretfinder
+cd ~/tools/secretfinder
+pip install -r requirements.txt
+cd ~/
 
 echo "if you want download full seclists or download raft folder which is i use mostly."
 echo "git clone https://github.com/danielmiessler/SecLists"
 
-
-
 echo -e "\n\n\nDone! All tools are set up in ~/tools\n\n"
-ls -la
+ls -la ~/tools/
+echo $PATH
+source ~/.bashrc
